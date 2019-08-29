f7eb21b91112aba897bdcbd712500a7a2b715b16
Jeremy <JeremyPlott@gmail.com>
2019-08-29 16:05:40 -0400

    Add project files.

---
+++ GenericCollections/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCollections {
+    class Program {
+        static void Main(string[] args) {
+
+            //5930    7643    6168    9491    5296    6762    7554    1751    9037    8939    9206    2021
+
+
+
+            5930
+            7643    
+            6168    
+            9491   
+            5296    
+            6762   
+            7554    
+            1751    
+            9037    
+            8939    
+            9206    
+            2021
+
+        }
+    }
+}
